9f53d3b0a2e5f19135f62316a95ea4b7ec53c8ba
Mampionona <ramamps33@gmail.com>
2025-12-13 02:11:05 +0300

    add parameter in the url, use default parameter if not exist

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    function index(): Response
+    function index(Request $request): Response
     {
-        return new Response('Hello World!');
+        $name = $request->query->get('name', 'World'); // Default to 'World' if 'name' parameter is not provided
+        return new Response("Hello $name");
     }
 }
